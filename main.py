bc092357987b27aee3a20964831f3fc5a934b6bd
Sean Riha <swriha@gmail.com>
2017-02-20 18:04:37 -0600

    Basic work finished based on ASCII chan video

--- main.py
+++ main.py
@@ -15,11 +15,71 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import cgi
+import os
+import re
 
-class MainHandler(webapp2.RequestHandler):
+from google.appengine.ext import db
+
+#setting up jinja
+template_dir = os.path.join(os.path.dirname(__file__), "templates")
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                                autoescape = True)
+
+
+class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+class BlogPost(db.Model):
+    title = db.StringProperty(required = True)
+    post_content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class MainPage(Handler):
+    def render_base(self, title="", post_content="", error=""):
+        blog_posts = db.GqlQuery("SELECT * FROM BlogPost ORDER BY created DESC")
+
+        self.render("base.html", title=title, post_content=post_content, error=error, blog_posts=blog_posts)
+
     def get(self):
-        self.response.write('Hello world!')
+        self.render_base()
+
+    def post(self):
+        title = self.request.get("title")
+        post_content = self.request.get("post_content")
+
+        if title and post_content:
+            p = BlogPost(title=title, post_content=post_content)
+            p.put()
+
+            self.redirect("/")
+        else:
+            error = "You need both a title and post content."
+            self.render_base(title, post_content, error)
+
+#class NewPost(Handler):
+#    def get(self):
+#        self.render("new-post.html")
+#
+#    def post(self):
+#        title = self.request.get("title")
+#        post = self.request.get("post")
+#
+#    if title and post:
+#        self.write("Thanks!")
+#    else:
+#        error = "You need both a title and post content."
+#        self.render("base.html", error = error)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainPage)
 ], debug=True)
